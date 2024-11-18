106a9da0c6aa33039214799003868863942f2dce
kuokne <kuokne@gmail.com>
2024-11-18 15:00:56 +0700

    sua bai tap

--- ss8-conditional-structrure-1/Bai_1/isLeapYear.js
+++ ss8-conditional-structrure-1/Bai_1/isLeapYear.js
@@ -1,3 +1,12 @@
+function displayYearCheck() {
+    let year = document.getElementById("yearCheck");
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0 )){
+        alert("Đây là năm nhuận!");
+    } else {
+        alert("Đây không phải là năm nhuận");
+    }
+}
+
 document.getElementById("click").addEventListener("click", function Check() {
     let year = document.getElementById("yearCheck");
     if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0 )){
@@ -6,5 +15,4 @@ document.getElementById("click").addEventListener("click", function Check() {
         alert("Đây không phải là năm nhuận");
     }
 })
-
 
