77913fa61000922eabd973e2d8e951d95b52fa3d
kuokne <kuokne@gmail.com>
2024-11-18 16:46:16 +0700

    commit bai 4

---
+++ ss8-conditional-structrure-1/Bai_4/exSwitchCase3.js
@@ -0,0 +1,11 @@
+let value = +prompt("Nhập vào 1 số", 0)
+switch (true) {
+    case value > 0:
+        alert(1);
+        break;
+    case value < 0 :
+        alert(-1);
+        break;
+    default:
+        alert(0);
+}
