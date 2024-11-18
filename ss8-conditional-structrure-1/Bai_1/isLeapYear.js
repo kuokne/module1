bd1d83523cca65967764af14fe70c219576cb1ba
kuokne <kuokne@gmail.com>
2024-11-18 14:55:32 +0700

    nop bai tap

---
+++ ss8-conditional-structrure-1/Bai_1/isLeapYear.js
@@ -0,0 +1,10 @@
+document.getElementById("click").addEventListener("click", function Check() {
+    let year = document.getElementById("yearCheck");
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0 )){
+        alert("Đây là năm nhuận!");
+    } else {
+        alert("Đây không phải là năm nhuận");
+    }
+})
+
+
