2383188b2b21a279b8fb1e7541b9527f218e8b21
kuokne <kuokne@gmail.com>
2024-11-18 15:34:59 +0700

    sua bai tap lan 2

--- ss8-conditional-structrure-1/Bai_1/isLeapYear.js
+++ ss8-conditional-structrure-1/Bai_1/isLeapYear.js
@@ -1,18 +1,23 @@
-function displayYearCheck() {
-    let year = document.getElementById("yearCheck");
-    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0 )){
-        alert("Đây là năm nhuận!");
+function checkLeapYear() {
+    let year = document.getElementById("yearCheck").value;
+    if (isNaN(year)  || year.length === 0) {
+        return "Vui lòng nhập vào số hợp lệ"
+    }
+    else if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0 )){
+        return "Đây là năm nhuận!";
     } else {
-        alert("Đây không phải là năm nhuận");
+        return "Đây không phải là năm nhuận";
     }
 }
 
 document.getElementById("click").addEventListener("click", function Check() {
-    let year = document.getElementById("yearCheck");
-    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0 )){
-        alert("Đây là năm nhuận!");
-    } else {
-        alert("Đây không phải là năm nhuận");
-    }
+    alert(checkLeapYear());
 })
 
+function displayYearCheck() {
+   alert(checkLeapYear());
+}
+
+function displayYearCheck2() {
+    return checkLeapYear();
+}
